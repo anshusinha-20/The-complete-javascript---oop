3b64b2241e230d1b0b74c0f3aee9d90d2d04c0b8
Anshu Sinha <anshujuly2@gmail.com>
2023-04-03 21:01:12 +0530

    Add a code to create the constructor function, attributes, methods and objects

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// 208. constructor functions and the new operator
+
+// constructor function
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // method
+  this.calcAge = function () {
+    console.log(2023 - this.birthYear);
+  };
+};
+
+// object
+const anshu = new Person("Anshu", 2003);
+console.log(anshu);
+anshu.calcAge();
+console.log(anshu instanceof Person);
