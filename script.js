728a2b0400dc945aac3799d25cf9f049885e5d9b
Anshu Sinha <anshujuly2@gmail.com>
2023-04-04 16:44:42 +0530

    modify

--- script.js
+++ script.js
@@ -2,19 +2,23 @@
 
 // 208. constructor functions and the new operator
 
-// constructor function
-const Person = function (firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// // constructor function
+// const Person = function (firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  // method
-  this.calcAge = function () {
-    console.log(2023 - this.birthYear);
-  };
-};
+//   // method
+//   this.calcAge = function () {
+//     console.log(2023 - this.birthYear);
+//   };
+// };
 
-// object
-const anshu = new Person("Anshu", 2003);
-console.log(anshu);
-anshu.calcAge();
-console.log(anshu instanceof Person);
+// // object
+// const anshu = new Person("Anshu", 2003);
+// console.log(anshu);
+// anshu.calcAge();
+// console.log(anshu instanceof Person);
+
+/////
+
+//
